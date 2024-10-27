72c706aa94a03b3760f70d23ead035625869c04e
Jiwon Park <xoxojw.dev@gmail.com>
2024-10-27 21:28:19 +0900

    chore: basic NextAuth callbacks for session and jwt handling
    - added `session` and `jwt` callbacks to extend session data
    - added `customField` example to demonstrate custom token handling

--- auth.ts
+++ auth.ts
@@ -6,6 +6,22 @@ import authConfig from "@/auth.config";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
 	adapter: PrismaAdapter(db),
+	callbacks: {
+		async session({ token, session }) {
+			console.log("SESSION TOKEN: ", { sessionToken: token, session });
+
+			if (session.user) {
+				session.user.customField = token.customField;
+			}
+
+			return session;
+		},
+		async jwt({ token, user }) {
+			console.log("TOKEN: ", { token });
+			token.customField = "test";
+			return token;
+		},
+	},
 	session: { strategy: "jwt" },
 	...authConfig,
 });
